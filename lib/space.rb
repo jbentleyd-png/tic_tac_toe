7c7af7578628c8ca0a2319836e47523d6002c67c
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-05-11 15:10:27 -0600

    Added row and column id to Space objects

--- lib/space.rb
+++ lib/space.rb
@@ -3,6 +3,17 @@ class Space
 
   def initialize(name)
     @name = name
+    @row = if name[0] == 'T'
+             'top'
+           else
+             name[0] == 'M' ? 'middle' : 'bottom'
+           end
+    @column = if name[1] == 'L'
+                'left'
+              else
+                name[1] == 'M' ? 'middle' : 'right'
+              end
+    puts "#{@row}, #{@column}"
     @@spaces_count += 1
   end
 
